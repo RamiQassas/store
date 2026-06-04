f8f05e89422262b208a51ce95cf208c05c466d39
RamiQassas <a0947991951@hotmail.com>
2026-06-05 02:59:45 +0300

    Update views.py

--- apps/support/views.py
+++ apps/support/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Q
+from django.utils import timezone
+from django.http import JsonResponse
 from .models import ChatRoom, ChatMessage, ChatCannedReply
 
 
