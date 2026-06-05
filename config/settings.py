3e4a2228a1d9f6a3ce547a2b8dcbe323a565d3c0
RamiQassas <a0947991951@hotmail.com>
2026-06-06 00:18:01 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -157,8 +157,8 @@ STATICFILES_STORAGE = (
     "whitenoise.storage.CompressedManifestStaticFilesStorage"
 )
 
+MEDIA_ROOT = "/var/data/media"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "/auth/login/"
