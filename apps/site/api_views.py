10e65b841316a2be894e512b3e01e13db83b46e5
RamiQassas <a0947991951@hotmail.com>
2026-06-10 00:45:14 +0300

    ت

--- apps/site/api_views.py
+++ apps/site/api_views.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from django.db import transaction
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
