c73cdfc006f06f1842ec50a99ad0f35f40c84128
RamiQassas <a0947991951@hotmail.com>
2026-06-11 00:35:56 +0300

    تحديث

--- apps/accounts/migrations/0023_temporarykycimage_alter_user_options_and_more.py
+++ apps/accounts/migrations/0023_temporarykycimage_alter_user_options_and_more.py
@@ -107,6 +107,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='user',
             name='username',
-            field=models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username'),
+            field=models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username'),
         ),
     ]
