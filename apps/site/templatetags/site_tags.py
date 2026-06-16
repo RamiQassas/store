c6aeee70f772d72a2f247bcacc182c47df0c285b
RamiQassas <a0947991951@hotmail.com>
2026-06-17 02:44:22 +0300

    Update site_tags.py

--- apps/site/templatetags/site_tags.py
+++ apps/site/templatetags/site_tags.py
@@ -100,6 +100,22 @@ def subtract(value, arg):
         return 0
 
 @register.filter
+def mul(value, arg):
+    """Multiplies the value by the argument."""
+    try:
+        return float(value) * float(arg)
+    except (ValueError, TypeError):
+        return 0
+
+@register.filter
+def div(value, arg):
+    """Divides the value by the argument."""
+    try:
+        return float(value) / float(arg)
+    except (ValueError, TypeError, ZeroDivisionError):
+        return 0
+
+@register.filter
 def split(value, arg):
     return value.split(arg)
 
